b66efbeff535c2429a06cfd835a7db4d49d3c2c2
sebacastillo <castilloclaudiosebastian@gmail.com>
2024-01-22 15:25:52 -0300

    update

---
+++ src/aisearch_async.py
@@ -0,0 +1,104 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from azure.core.credentials import AzureKeyCredential
+from azure.search.documents import SearchClient
+from azure.search.documents.models import VectorizedQuery, VectorFilterMode
+from langchain_community.embeddings import AzureOpenAIEmbeddings
+from openai import AzureOpenAI
+from semantic_kernel.skill_definition import (
+    sk_function,
+    sk_function_context_parameter,
+)
+
+# Load environment variables
+load_dotenv()
+
+class AISearch:
+    def __init__(self, index_name="finance-bench-small-sk"):
+        # Environment variable definitions
+        self.azure_ai_search_endpoint = os.getenv("AZURE_AISEARCH_ENDPOINT")
+        self.azure_ai_search_api_key = os.getenv("AZURE_AISEARCH_API_KEY")
+        self.azure_openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+        self.azure_openai_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+        self.azure_openai_api_version = os.getenv("AZURE_OPENAI_API_VERSION")
+        self.azure_openai_embeddings_model_name = os.getenv("AZURE_OPENAI_EMBEDDINGS_MODEL_NAME")
+
+        # Azure credentials and clients
+        credential = AzureKeyCredential(self.azure_ai_search_api_key)
+        self.search_client = SearchClient(self.azure_ai_search_endpoint, index_name, credential=credential)
+        self.openai_client = AzureOpenAI(
+            api_key=self.azure_openai_api_key,
+            api_version="2023-05-15",
+            azure_endpoint=self.azure_openai_endpoint
+        )
+        self.embeddings = AzureOpenAIEmbeddings(
+            azure_deployment="text-embedding-ada-002",
+            openai_api_version="2023-05-15",
+            chunk_size=10
+        )
+
+    def generate_embeddings(self, text):
+        return self.openai_client.embeddings.create(input=[text], model=self.azure_openai_embeddings_model_name).data[0].embedding
+
+
+    @sk_function(
+        description="This function answer all questions from the user related to finance documents",
+        name="index_search",
+        input_description="The query may be any general question that may be found in Documents (context).",
+    )
+    async def search(self, query="Revenue of Microsoft", k_nearest_neighbors=5, top=5, embed_field='Embedding', select_fields=None, query_type="simple", filter=None):
+        vector_query = VectorizedQuery(vector=self.generate_embeddings(query), k_nearest_neighbors=k_nearest_neighbors, fields=embed_field)
+        
+        if query_type == "simple":
+            results = self.search_client.search(
+                search_text=None,
+                vector_queries=[vector_query],
+                select=select_fields if select_fields else ["Text", "Id"],
+                top=top
+            )
+
+        elif query_type == "hybrid":
+            results = self.search_client.search(
+                search_text=query,
+                vector_queries=[vector_query],
+                select=select_fields if select_fields else ["Text", "Id"],
+                top=top
+            )
+
+        elif query_type == "hybrid_wfilter":
+            results = self.search_client.search(
+                search_text=None,
+                vector_queries=[vector_query],
+                vector_filter_mode=VectorFilterMode.PRE_FILTER,
+                filter=filter,
+                select=select_fields if select_fields else ["Text", "Id"],
+                top=top
+            )
+        else:
+            raise ValueError("Invalid query type. Choose from 'simple', 'hybrid', 'hybrid_wfilter'.")
+
+        return results
+
+#Modify the main code to handle async
+async def main():
+    ai_search = AISearch()
+
+    hybrid_search_results = await ai_search.search(query="Revenue of Microsoft", query_type="hybrid", top=3)
+    print("\nHybrid Search Results:")
+    docs = [result['Text'] for result in hybrid_search_results]
+
+    def format_documents(doc_list):
+        formatted_string = ""
+        for i, doc in enumerate(doc_list):
+            formatted_string += f"###\n<doc{i+1}> {doc}\n"
+        return formatted_string
+    
+    documents = format_documents(docs)
+    print(documents)
+
+
+if __name__ == "__main__":
+    # Load environment variables
+    load_dotenv()
+    asyncio.run(main())
